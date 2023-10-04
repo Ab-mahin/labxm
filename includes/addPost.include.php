3cfc1198cdd67b41ef472d5b6173e63fd91f5897
AST-TheCoder <tonmoy.cse5.bu@gmail.com>
2023-10-04 15:42:33 +0600

    Requirements Changed

--- includes/addPost.include.php
+++ includes/addPost.include.php
@@ -11,12 +11,7 @@
     $content=$_POST['content'];
     $date=date("Y-m-d");
 
-    $filename = $_FILES["img"]["name"];
-    $tempname = $_FILES["img"]["tmp_name"];
-    $folder = "/opt/lampp/htdocs/labxm/img/" . basename($_FILES["img"]["name"]);
-    move_uploaded_file($tempname, $folder);
-
-    //mysqli_query($conn, "INSERT INTO `posts` (`pId`, `cId`, `title`, `content`, `modDate`) VALUES (NULL, '$cId', '$title', '$content', '$date');");
-    //header('location: ../home.php');
+    mysqli_query($conn, "INSERT INTO `posts` (`pId`, `cId`, `title`, `content`, `modDate`) VALUES (NULL, '$cId', '$title', '$content', '$date');");
+    header('location: ../home.php');
 
 ?>
